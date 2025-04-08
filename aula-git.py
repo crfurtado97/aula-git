3daf3f164ef38ad4d2c539e931b0c7c014820623
crfurtado97 <cristianofurtado2010@hotmail.com>
2025-04-08 20:06:23 -0300

    Projeto criado

---
+++ aula-git.py
@@ -0,0 +1,13 @@
+
+idade_pessoa = int(input("Digite sua idade na tela : "))
+
+classificacao_conteudo = idade_pessoa >= 16
+
+print ("Liberado o acesso para filme" , classificacao_conteudo) 
+
+
+
+
+
+
+
